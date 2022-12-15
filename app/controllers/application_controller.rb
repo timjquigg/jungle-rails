102641d3359b49f888ee9eea21ebd08bf077de83
Tim Quigg <timjquigg@gmail.com>
2022-12-15 14:20:16 -0700

    Add user authentication. Create signup and login page. Add login/sign up to nav bar. When user logged in displays user email and logout button.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,14 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  # Make the current_user method available to views also, not just controllers:
+  helper_method :current_user
+  
+  # Define the current_user method:
+  def current_user
+    # Look up the current user based on user_id in the session cookie:
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,21 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#index'
 
+  # sign up page with form:
+	get 'users/new' => 'users#new', as: :new_user
+	
+	# create (post) action for when sign up form is submitted:
+	post 'users' => 'users#create'
+
+  # log in page with form:
+	get '/login'     => 'sessions#new'
+	
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+	
+	# delete action to log out:
+	delete '/logout' => 'sessions#destroy'  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
